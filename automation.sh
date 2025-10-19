fe8e4e58bf967d996c86ea817a70f87836a696d1
Miyata-lee <ashiqmeeran786@gmail.com>
2025-10-19 10:45:31 +0530

    basich script

--- automation.sh
+++ automation.sh
@@ -4,8 +4,7 @@ sudo useradd lee -m -s /bin/bash && echo "lee:123" | sudo chpasswd
 for user in {1..5};
 do  
     username="base$user"
-    useradd -m "$username"
-    echo "$username:123" | chpasswd
+    sudo useradd -m "$username" && echo "$username:123" | chpasswd
 done
 
 sudo systemctl stop httpd || echo "httpd not installed"
